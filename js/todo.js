1eed8553556af70ea5323f8cc95858990ae71d9a
bioroid17 <trace1997@ajou.ac.kr>
2023-08-12 05:32:20 +0900

    TO DO LIST - Adding To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDolist = document.getElementById("todo-list");
 
+function paintToDo(newToDo) {
+	const li = document.createElement("li");
+	const span = document.createElement("span");
+	li.appendChild(span);
+	span.innerText = newToDo;
+	toDolist.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
 	event.preventDefault();
 	const newToDo = toDoInput.value;
-	console.log(newToDo, toDoInput.value);
 	toDoInput.value = "";
-	console.log(newToDo, toDoInput.value);
+	paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
