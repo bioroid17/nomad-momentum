f4a0b4e962aca3ebcdb6656c35fb622ae05beaf2
bioroid17 <trace1997@ajou.ac.kr>
2023-08-12 03:55:33 +0900

    TO DO LIST - Setup

---
+++ js/todo.js
@@ -0,0 +1,13 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDolist = document.getElementById("todo-list");
+
+function handleToDoSubmit(event) {
+	event.preventDefault();
+	const newToDo = toDoInput.value;
+	console.log(newToDo, toDoInput.value);
+	toDoInput.value = "";
+	console.log(newToDo, toDoInput.value);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
